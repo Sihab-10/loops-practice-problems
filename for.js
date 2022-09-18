96d23ca9956887206322670870ed0501349311f6
Sihab-10 <sihabsarar10@gmail.com>
2022-09-18 17:30:23 +0600

    for loop added

---
+++ for.js
@@ -0,0 +1,38 @@
+// while loop
+// var roastGiven = 0;
+// while (roastGiven < 7) {
+//   console.log("roast den, please !!");
+//   roastGiven++;
+// }
+
+//for loop
+
+// for (roastGiven = 0; roastGiven < 7; roastGiven++) {
+//   console.log("roast den, please!!");
+// }
+
+// simple version of for loop
+/* console.log("Simple for loop");
+for (i = 0; i < 7; i++) {
+  console.log(i);
+} */
+
+/*  
+
+target: display 1 to 10 using for loop
+
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+*/
+
+// target: display 1 to 20 odd numbers
+/* for (var i = 1; i <= 20; i = i + 2) {
+  console.log(i);
+} */
+
+// target: display 1 to 20 odd numbers
+for (var i = 0; i <= 20; i = i + 2) {
+  console.log(i);
+}
