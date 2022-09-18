57c58fda17c81d626a89db3b256094307b37f0d5
Sihab-10 <sihabsarar10@gmail.com>
2022-09-18 15:49:30 +0600

    loop starts

---
+++ revision.js
@@ -0,0 +1,28 @@
+// string type variable
+var bottle = "water";
+var bottleColor = "yellow";
+var penColor = "Blue";
+// number type variable
+var sunglassPrice = 120;
+// Boolean type variable
+var isExpensive = true;
+
+// array
+
+var tableItems = ["bottle", "pen", "sunglass", "mouse", "paper"];
+
+var item = tableItems[3];
+console.log(item);
+tableItems[4] = "notebook";
+console.log(tableItems);
+var penIndex = tableItems.indexOf("pen");
+console.log(penIndex);
+
+// push , pop
+
+// conditionals
+
+if (tableItems.length > 3) {
+} else if (tableItems[3] == "pen") {
+} else {
+}
