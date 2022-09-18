7e518a82a02b2b4f9ade649993fc0ec8814879e7
Sihab-10 <sihabsarar10@gmail.com>
2022-09-18 16:15:47 +0600

    while loop upadated

--- while.js
+++ while.js
@@ -1,7 +1,15 @@
 // if (condition) {
 
 // }
+/*
+=============Structure of While loop=================
+1. Loop variable
+2. condition inside while
+3. Loop Body 
+4. Change the loop variable
 
+
+*/
 var roastGiven = 0;
 while (roastGiven < 7) {
   console.log("roast den, Please");
