e6a23e50fe841c6726805067aa9eba06407d5f42
Sihab-10 <sihabsarar10@gmail.com>
2022-09-22 17:53:58 +0600

    array with loop added

---
+++ array-loop.js
@@ -0,0 +1,14 @@
+/* for (var i = 1; i <= 10; i++) {
+  console.log(i);
+} */
+// display every elements of an array
+/* var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+for (var i = 0; i < numbers.length; i++) {
+  var number = numbers[i];
+  console.log(number);
+} */
+var items = ["bottle", "dates fruits", "pen", "scale", "chanachur"];
+for (var i = 0; i < items.length; i++) {
+  var item = items[i];
+  console.log(item);
+}
