ab8da36a9af1ad71de7e77738b70e9eeb150b708
Sihab-10 <sihabsarar10@gmail.com>
2022-09-25 08:14:50 +0600

    grading problem in js added

---
+++ grading.js
@@ -0,0 +1,71 @@
+var myself = 85;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+
+if (myself >= 80) {
+  console.log("You Got A Grade");
+} else if (myself >= 60) {
+  console.log("You Got B Grade");
+} else if (myself >= 50) {
+  console.log("You Got C Grade");
+} else if (myself >= 40) {
+  console.log("You Got D Grade");
+} else if (myself <= 39) {
+  console.log("You Got F Grade");
+} else {
+  console.log("Your Marks are invalid");
+}
+if (tom >= 80) {
+  console.log("tom Got A Grade");
+} else if (tom >= 60) {
+  console.log("tom Got B Grade");
+} else if (tom >= 50) {
+  console.log("tom Got C Grade");
+} else if (tom >= 40) {
+  console.log("tom Got D Grade");
+} else if (tom <= 39) {
+  console.log("tom Got F Grade");
+} else {
+  console.log("tom Marks are invalid");
+}
+if (jane >= 80) {
+  console.log("jane Got A Grade");
+} else if (jane >= 60) {
+  console.log("jane Got B Grade");
+} else if (jane >= 50) {
+  console.log("jane Got C Grade");
+} else if (jane >= 40) {
+  console.log("jane Got D Grade");
+} else if (jane <= 39) {
+  console.log("jane Got F Grade");
+} else {
+  console.log("Jane Marks are invalid");
+}
+if (peter >= 80) {
+  console.log("peter Got A Grade");
+} else if (peter >= 60) {
+  console.log("peter Got B Grade");
+} else if (peter >= 50) {
+  console.log("peter Got C Grade");
+} else if (peter >= 40) {
+  console.log("peter Got D Grade");
+} else if (peter <= 39) {
+  console.log("peter Got F Grade");
+} else {
+  console.log("Your Marks are invalid");
+}
+if (john >= 80) {
+  console.log("john Got A Grade");
+} else if (john >= 60) {
+  console.log("john Got B Grade");
+} else if (john >= 50) {
+  console.log("john Got C Grade");
+} else if (john >= 40) {
+  console.log("john Got D Grade");
+} else if (john <= 39) {
+  console.log("john Got F Grade");
+} else {
+  console.log("john Marks are invalid");
+}
